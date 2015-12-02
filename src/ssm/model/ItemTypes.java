d2fb38ca584452d42ce38b5b5b8c74a584fcf31e
ginahvitus <robert.sheynin@stonybrook.edu>
2015-12-01 22:45:59 -0500

    Initial Committ
    -Created Classes to refer to material
    -Created Enum for these classes
    -Changed GUI layout
    
    
    TODO 
    -Functionality

---
+++ src/ssm/model/ItemTypes.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ssm.model;
+
+/**
+ *
+ * @author Huxton
+ */
+public enum ItemTypes {
+
+    Text,
+    Video,
+    List,
+    SlideShow,
+    Image,
+
+}
